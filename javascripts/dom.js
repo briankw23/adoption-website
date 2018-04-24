e817fdd37895ad21ea37de44909aab5ec5c5c432
Brian Wright <briankw23@gmail.com>
2018-04-24 16:49:17 -0500

    fixed bootstrap

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,11 +1,17 @@
 let domString = '';
 const buildPets = (data) => {
   for (let i = 0; i < data.length; i++) {
-    domString += `<div class="col-md-4">`;
-    domString += `<h1>${data[i].name}</h1>`;
+    domString += `<div class="col-md-4 text-center">`;
+    domString += `<div class="panel panel-default">`;
+    domString += `<div class="panel-heading">`;
+    domString += `<h1 class="panel-title">${data[i].name}</h1>`;
+    domString += `</div>`;
+    domString += `<div class="panel-body">`;
     domString += `<img class='img-rounded' src='${data[i].imageUrl}'>;`;
     domString += `<p>${data[i].specialSkill}<p>`;
     domString += `<p id="${data[i].type}">${data[i].type}</p>`;
+    domString += `</div>`;
+    domString += `</div>`;
     domString += `</div>`;
   }
   printToDom(domString,'main');
@@ -22,11 +28,17 @@ const printToDom = (string,divId) => {
 let catDomString = '';
 const catCards = (cats) => {
   for (let i = 0; i < cats.length; i++) {
-    catDomString += `<div class="col-md-4">`;
-    catDomString += `<h1>${cats[i].name}</h1>`;
+    catDomString += `<div class="col-md-4 text-center">`;
+    catDomString += `<div class="panel panel-default">`;
+    catDomString += `<div class="panel-heading">`;
+    catDomString += `<h1 class="panel-title">${cats[i].name}</h1>`;
+    catDomString += `</div>`;
+    catDomString += `<div class="panel-body">`;
     catDomString += `<img class='img-rounded' src='${cats[i].imageUrl}'>;`;
     catDomString += `<p>${cats[i].specialSkill}<p>`;
     catDomString += `<p id="${cats[i].type}">${cats[i].type}</p>`;
+    catDomString += `</div>`;
+    catDomString += `</div>`;
     catDomString += `</div>`;
   };
 };
@@ -37,11 +49,17 @@ const catsPrintToDom = (e) => {
 let dogDomString = '';
 const dogCards = (dogs) => {
   for (let i = 0; i < dogs.length; i++) {
-    dogDomString += `<div class="col-md-4">`;
-    dogDomString += `<h1>${dogs[i].name}</h1>`;
+    dogDomString += `<div class="col-md-4 text-center">`;
+    dogDomString += `<div class="panel panel-default">`;
+    dogDomString += `<div class="panel-heading">`;
+    dogDomString += `<h1 class="panel-title">${dogs[i].name}</h1>`;
+    dogDomString += `</div>`;
+    dogDomString += `<div class="panel-body">`;
     dogDomString += `<img class='img-rounded' src='${dogs[i].imageUrl}'>;`;
     dogDomString += `<p>${dogs[i].specialSkill}<p>`;
     dogDomString += `<p id="${dogs[i].type}">${dogs[i].type}</p>`;
+    dogDomString += `</div>`;
+    dogDomString += `</div>`;
     dogDomString += `</div>`;
   };
   console.log('catDomString', dogDomString);
@@ -54,16 +72,18 @@ let dinoDomString = '';
 const dinoCards = (dinos) => {
   console.log('domdinos',dinos);
   for (let i = 0; i < dinos.length; i++) {
-    // dinoDomString += `<div class="col-md-4">`;
+    dinoDomString += `<div class="col-md-4 text-center">`;
     dinoDomString += `<div class="panel panel-default">`;
-    dinoDomString += `<div class="panel-heading"><h1>${dinos[i].name}</h1></div>`;
+    dinoDomString += `<div class="panel-heading">`;
+    dinoDomString += `<h1 class="panel-title">${dinos[i].name}</h1>`;
+    dinoDomString += `</div>`;
     dinoDomString += `<div class="panel-body">`;
     dinoDomString += `<img class='img-rounded' src='${dinos[i].imageUrl}'>;`;
     dinoDomString += `<p>${dinos[i].specialSkill}<p>`;
     dinoDomString += `<p id="${dinos[i].type}">${dinos[i].type}</p>`;
     dinoDomString += `</div>`;
     dinoDomString += `</div>`;
-    // dinoDomString += `</div>`;
+    dinoDomString += `</div>`;
   };
   console.log('catDomString', dinoDomString);
 };
